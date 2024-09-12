e17d1869c63dc5e0106b4b258242084ef6d67082
Jeanettetech <jeanetteintech@gmail.com>
2024-09-12 15:57:55 +0100

    exercises

--- exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
+++ exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
@@ -1,6 +1,8 @@
 package com.cbfacademy;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,25 +11,74 @@ public class FlowControlExercises {
     public List<String> fizzBuzz(List<Integer> numbers) {
         // TODO - Implement this method such that
         List<String> result = new ArrayList<String>();
+
         // it creates a list where for each element of the input list ${numbers}
-        // - if the ${element} is divisible by 3, it adds “Fizz” to the list
-        // - if the ${element} is divisible by 5, it adds “Buzz” to the list
-        // - if the ${element} is divisible by both 3 and 5, it adds “FizzBuzz” to the
-        // list
-        // - it adds the element to the list in any other case
-        // - it returns the constructed list
-        throw new RuntimeException("Not implemented");
+
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) % 3 == 0 && numbers.get(i) % 5 == 0) {
+                result.add("FizzBuzz");
+            } else if (numbers.get(i) % 3 == 0) {
+                result.add("Fizz");
+            } else if (numbers.get(i) % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(numbers.get(i).toString());
+            }
+        }
+
+        return result;
+
+        // throw new RuntimeException("Not implemented");
     }
 
     public String whichMonth(Integer number) {
-        // TODO - Implement this method such that
-        // - it returns the month corresponding to the input ${number}
-        // - if the ${number} is invalid, the method should return "Invalid month
-        // number"
-        throw new RuntimeException("Not implemented");
+
+        if (number == null || number < 1 || number > 12) {
+            return "Invalid month number";
+        }
+
+        String[] months = {
+                "January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+        };
+
+        return months[number - 1];
+
+        // throw new RuntimeException("Not implemented");
     }
 
     public Map<String, Integer> sumOfOddsAndSumOfEvens() {
+
+        // Create and initialize a list of numbers from 1 to 100
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 100; i++) {
+            numbers.add(i);
+        }
+
+        // Determine the sum of all even numbers
+        int sumOfEvens = 0;
+        int sumOfOdds = 0;
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                sumOfEvens += number;
+            } else {
+                sumOfOdds += number;
+            }
+        }
+
+        // Create a map to store the results
+        Map<String, Integer> result = new HashMap<>();
+        result.put("SumOfEvens", sumOfEvens);
+        result.put("SumOfOdds", sumOfOdds);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        NumberSums numsum = new NumberSums();
+        Map<String, Integer> result = numsum.sumOfOddsAndSumOfEvens();
+        System.out.println(result);
+
         // TODO - Implement this method such that
         // - creates and initialises a list of 100 numbers - from 1 to 100
         // - determines the sum of all the even numbers in the list
@@ -38,10 +89,16 @@ public class FlowControlExercises {
     }
 
     public List<Integer> reverse(ArrayList<Integer> numbers) {
+
+        List<Integer> reversedList = new ArrayList<>(numbers);
+        // Use Collections.reverse to reverse the list
+        Collections.reverse(reversedList);
+        return reversedList;
+
         // TODO - Implement this method such that
         // - it takes an array list of integers
         // - it returns the list in reverse order
-        throw new RuntimeException("Not implemented");
+        // throw new RuntimeException("Not implemented");
     }
 
     public String getName() {
---
+++ exercises/flow-control/src/main/java/com/cbfacademy/NumberSums.java
@@ -0,0 +1,12 @@
+package com.cbfacademy;
+
+import java.util.Map;
+
+public class NumberSums {
+
+	public Map<String, Integer> sumOfOddsAndSumOfEvens() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'sumOfOddsAndSumOfEvens'");
+	}
+
+}
