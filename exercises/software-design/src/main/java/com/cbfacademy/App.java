cb834edc5f9a91e5a835608f33634c4970ad34c8
Jeanettetech <jeanetteintech@gmail.com>
2024-10-20 16:18:05 +0100

    IOC Container

--- exercises/software-design/src/main/java/com/cbfacademy/App.java
+++ exercises/software-design/src/main/java/com/cbfacademy/App.java
@@ -1,11 +1,10 @@
 package com.cbfacademy;
 
 public class App {
-    public static void main(String[] args) {
-        Player player1 = PlayerFactory.create();// instance creation
-        Player player2 = PlayerFactory.create();
-
-        Game game = GameFactory.create(player1, player2);
+    public static void main(String[] args) {// Add a call to initialiseContainer in the main method of App, before any
+                                            // factory method calls.
+        SimpleContainer container = initialiseContainer();
+        Game game = container.get(Game.class);
         String winner = game.play();
 
         if (winner == null) {
@@ -13,5 +12,18 @@ public class App {
         } else {
             System.out.println(winner + " wins!");
         }
+    }
+
+    private static SimpleContainer initialiseContainer() {// method to register mappings for the Game and Player
+                                                          // interfaces with their concrete implementations in the
+                                                          // container, e.g. container.register(Game.class,
+                                                          // DiceGame.class)
+        SimpleContainer container = new SimpleContainer();
+
+        container.register(Game.class, DiceGame.class);
+        container.register(Player.class, DicePlayer.class);
+
+        return container;
+
     }
 }
